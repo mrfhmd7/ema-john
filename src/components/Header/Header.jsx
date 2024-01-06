7db09b76e93a8029268c1ea73769c1e072063352
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-06 23:26:33 +0600

    nav bar added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
 
 const Header = () => {
      return (
-          <div>
-               <h2>This is header.</h2>
-          </div>
+          <nav className='header'>
+               <img src={logo} alt="" />
+               <div>
+                    <a href="/order">Order</a>
+                    <a href="/review">Order Review</a>
+                    <a href="/inventory">Manage Inventory</a>
+                    <a href="/login">Login</a>
+               </div>
+          </nav>
      );
 };
 
