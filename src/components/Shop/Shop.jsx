d10753f11141f81a258365e4dfc37cc7a93512fd
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-06 23:40:14 +0600

    Shop component added

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+     return (
+          <div className='shop-container'>
+               <div className='product-container'>
+                    <h2>Product is coming here.</h2>
+               </div>
+               <div className='cart-container'>
+                    <h4>Order summary</h4>
+               </div>
+          </div>
+     );
+};
+
+export default Shop;
