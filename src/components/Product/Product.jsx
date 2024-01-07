f1f7d2044de3908d0aeed96342bf8f0675380547
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-07 22:59:34 +0600

    price and details added and fix some img link

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-     const {img, name, seller, quantity, price} = props.product;
-     console.log(props.product);
+     const {img, name, seller, ratings, price} = props.product;
+     // console.log(props.product);
      return (
           <div className='product'>
                <img src={img} alt="" />
-               <h6>{name}</h6>
+               <div className='product-info'>
+                    <h6 className='product-name'>{name}</h6>
+                    <p className='product-price'>Price: ${price}</p>
+                    <p>Manufacturer: {seller}</p>
+                    <p>Ratings: {ratings} stars</p>
+               </div>
           </div>
      );
 };
