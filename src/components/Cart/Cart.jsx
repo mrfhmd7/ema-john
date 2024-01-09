65c87c4ea54dd89413f21f1029658cc01c66a434
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-09 21:07:19 +0600

    calculate total price in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = (props) => {
-     console.log(props.cart);
-     const {cart} = props;
+     // console.log(props.cart);
+     const { cart } = props;
+     
+     let total = 0;
+     for (const product of cart) {
+          total = total + product.price;
+          // console.log(total);
+     }
+     
      return (
           <div className='cart'>
                <h5>Order summary</h5>
                <p>Selected item: {cart.length}</p>
-               <p>Total price: $</p>
+               <p>Total price: ${total}</p>
                <p>Total shipping charge: $</p>
                <p>Tax: $</p>
                <h6>Grand Total: $</h6>
