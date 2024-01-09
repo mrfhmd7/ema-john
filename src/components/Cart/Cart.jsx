316c2b35e2bc1b0a984aec0eb0128127cca1cc67
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-09 21:02:07 +0600

    added information in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
-     console.log(props);
+     console.log(props.cart);
      const {cart} = props;
      return (
-          <div>
-               <h4>Order summary</h4>
+          <div className='cart'>
+               <h5>Order summary</h5>
                <p>Selected item: {cart.length}</p>
+               <p>Total price: $</p>
+               <p>Total shipping charge: $</p>
+               <p>Tax: $</p>
+               <h6>Grand Total: $</h6>
           </div>
      );
 };
