027b679aa862a8ed60c5646284cbdad98f79a294
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-10 10:32:40 +0600

    added product in local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb } from '../../utilities/fakedb';
 
 const Shop = () => {
 
@@ -12,6 +13,7 @@ const Shop = () => {
      const handleAddToCart = (product) => {
           const newCart = [...cart, product];
           setCart(newCart);
+          addToDb(product.id);
      }
 
      useEffect(() => {
