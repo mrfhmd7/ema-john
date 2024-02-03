01458e2ab8373aae755a3fb3677bb786d4c0237d
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-03 23:52:51 +0600

    react router added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
           <nav className='header'>
                <img src={logo} alt="" />
                <div>
-                    <a href="/order">Order</a>
-                    <a href="/review">Order Review</a>
+                    <a href="/">Shop</a>
+                    <a href="/orders">Order Review</a>
                     <a href="/inventory">Manage Inventory</a>
                     <a href="/login">Login</a>
                </div>
