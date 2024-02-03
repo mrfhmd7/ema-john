326f2627a575e6eeb1037a81a046624b9e37af6e
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-04 00:20:11 +0600

    components added

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,9 @@ import {
 } from "react-router-dom";
 import Shop from './components/Shop/Shop.jsx';
 import Home from './components/Layout/Home.jsx';
+import Orders from './components/Orders/Orders.jsx';
+import Inventory from './components/Inventory/Inventory.jsx';
+import Login from './components/Login/Login.jsx';
 
 const router = createBrowserRouter([
   {
@@ -18,6 +21,18 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Shop />,
+      },
+      {
+        path: '/orders',
+        element: <Orders/>
+      },
+      {
+        path: '/inventory',
+        element: <Inventory/>
+      },
+      {
+        path: '/login',
+        element: <Login/>
       }
     ]
   }
@@ -25,6 +40,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
