95643aa0658823d93aaffd1d6dfb6c4209195260
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-04 00:24:42 +0600

    update href to Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from '../../images/Logo.svg';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
      return (
           <nav className='header'>
                <img src={logo} alt="" />
                <div>
-                    <a href="/">Shop</a>
-                    <a href="/orders">Order Review</a>
-                    <a href="/inventory">Manage Inventory</a>
-                    <a href="/login">Login</a>
+                    <Link to="/">Shop</Link>
+                    <Link to="/orders">Order Review</Link>
+                    <Link to="/inventory">Manage Inventory</Link>
+                    <Link to="/login">Login</Link>
                </div>
           </nav>
      );
