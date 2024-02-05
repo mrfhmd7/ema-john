a8585673cf6d01654851ef9c1ea403cb245ab965
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-05 22:47:43 +0600

    show order summary in order page

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,15 +3,15 @@ import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
-     const products = useLoaderData();
+     const cart = useLoaderData();
 
      return (
           <div className='shop-container'>
                <div className='product-container'>
-                    <h2>Ordered Product:{products.length}</h2>
+                    <h2>Ordered Product: {cart.length}</h2>
                </div>
                <div className='cart-container'>
-                    <Cart cart={[]} />
+                    <Cart cart={cart} />
                </div>
           </div>
      );
