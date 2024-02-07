4d25f9e0b614bc46ee305652929951bfe15e0279
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-07 22:49:38 +0600

    import icon

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -37,8 +37,13 @@ const Orders = () => {
                          cart={cart}
                          handleClearCart={handleClearCart}
                     >
-                         <Link to='/checkout'>
-                              <button className='btn-review-proceed'>Proceed Checkout</button>
+                         <Link className='btn-link' to='/checkout'>
+                              <button className='btn-review-proceed'>
+                                   <span>Proceed Checkout</span>
+                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+                                   </svg>
+                              </button>
                          </Link>
                     </Cart>
                </div>
